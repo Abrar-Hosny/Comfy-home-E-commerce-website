87c9a2984648db7dbd415331f18b8d1544b558de
Abrar-Hosny <abrarhosny19@gmail.com>
2023-12-05 22:41:43 +0200

    validation of the inputs is done

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$server="localhost";
+$username= "root" ;
+$password=""; 
+$database="signup";
+$connection=mysqli_connect("$server", "$username","$password");
+
+
+?>
---
+++ process-signup.php
@@ -0,0 +1,51 @@
+<?php
+
+// validate the name
+
+
+if(empty($_POST["name"])){
+die("name is required");
+}
+
+// validate the email 
+// THIS return false if the email is not valid address
+if(!filter_var($_POST["email"] , FILTER_VALIDATE_EMAIL)) {
+    die("valid email is required");
+
+}
+
+// validate password first we will need to know that 
+// is 8 characters
+
+if(strlen(($_POST["password"]))<8){
+    die("Password must be 8 characters");
+}
+// this matches the value to regular expression
+// upper and lower
+if(!preg_match("/[a-z]/i" ,$_POST["password"] )){
+    die("Password must contain at least one letter");
+
+}
+
+if(!preg_match("/[0-9]/" ,$_POST["password"] )){
+    die("Password must contain at least one number");
+
+}
+
+
+
+if($_POST["password"] !== $_POST["repeatpassword"]){
+    die("donot match");
+}
+
+
+
+
+
+
+
+
+
+// print_r($_POST);
+
+?>
