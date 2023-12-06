191d360df7a2cef9e65f85c0d1ed061b3f4065a0
Abrar-Hosny <abrarhosny19@gmail.com>
2023-12-07 01:30:52 +0200

    scroll reveal works

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const scrollRevealOption={
     distance : "50px",
     origin : "bottom",
-    duration :1000 }
+    duration :1000 };
 
     // header container
 
-    scrollReveal().reveal("")
+    ScrollReveal().reveal(".header_container h1" , {
+        ...scrollRevealOption,
+    });
+
+    ScrollReveal().reveal(".header_container p" , {
+        ...scrollRevealOption,
+        delay : 500,
+    });
+
+    ScrollReveal().reveal(".header_container a" , {
+        ...scrollRevealOption,
+        delay:1000
+    });
+
+ 
