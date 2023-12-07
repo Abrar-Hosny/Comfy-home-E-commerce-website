311293c92f7c101569aec6dba8027cfa1b0f9423
Abrar-Hosny <abrarhosny19@gmail.com>
2023-12-07 02:04:52 +0200

    login is working good 🐝

--- loginform.php
+++ loginform.php
@@ -1,3 +1,53 @@
+<?php
+// when click and the form is sumitted the method will be return
+// here to check that we sumit the form
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    // connecting the database 
+
+    $mysqli= require __DIR__ ."/database.php" ;
+    // here we will select the record based on the email address
+//     SELECT * FROM user WHERE email = '%s':
+
+// This is a SQL query that selects all columns (*) from the user table where the value in the email column matches a specific email address. The %s is a placeholder for a string, and it will be replaced by the actual email address.
+// sprintf:
+
+// The sprintf function is a PHP function used to format strings. It takes a format string as its first argument, followed by a variable number of additional arguments. The placeholders in the format string (like %s) are replaced by the values of the additional arguments.
+    $sql =sprintf("SELECT * FROM user WHERE email = '%s' " , 
+    
+    $mysqli->real_escape_string( $_POST["email"]));
+
+    $result=$mysqli->query($sql);
+
+    // this returns the row of existing email and stored in associative array
+    $user=$result ->fetch_assoc();
+// if the user is exist
+// will return null if the user is not exist 
+    if($user){
+        
+        // here we will verify the paasword that the user entered in the form
+        // with the ones that in the table  password hash
+
+        if(password_verify($_POST["password"],$user["password_hash"])){
+            die("login success");
+        }
+        else{
+            die("login bad");
+
+
+        }
+
+    }
+
+
+}
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,16 +77,16 @@
 <!-- instead of submit the form into separate script like sign up 
 here we willnot need to add action here
 -->
-<form class="space-y-6" method="$_POST">
+<form class="space-y-6" method="POST" novalidate>
         <h5 class="text-xl font-medium text-gray-900 dark:text-white">login to our platform</h5>
        
         <div>
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
+            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" >
         </div>
         <div>
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" >
         </div>
         
         <div class="flex items-start">
